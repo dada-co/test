5325c2caeefe6e12456787026aca66dd60ef208c
dada-co <nirco.cohen@gmail.com>
2024-09-02 13:34:03 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ with st.echo():
 
     options = Options()
     options.add_argument("--disable-gpu")
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
 
     driver = get_driver()
     driver.get("https://www.cnn.com")
